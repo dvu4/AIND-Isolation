06169924438c533c085d8c0efb13695117bda875
Duc Vu <dvu4@uic.edu>
2017-04-24 04:12:04 -0500

    Add files via upload

--- run_multiple.sh
+++ run_multiple.sh
@@ -4,24 +4,20 @@
 #    python tournament.py
 #done
 
-#echo "Running heuristic: heuristic_score_player_moves"
-#touch result/heuristic_score_player_moves.txt
-#python tournament.py heuristic_score_player_moves >> result/heuristic_score_player_moves.txt
+echo "Running heuristic: heuristic_score_player_moves"
+touch result/heuristic_score_player_moves.txt
+python tournament.py heuristic_score_player_moves >> result/heuristic_score_player_moves.txt
 
 
-#echo "Running heuristic: heuristic_score_normal_difference_moves"
-#touch result/heuristic_score_normal_difference_moves.txt
-#python tournament.py heuristic_score_normal_difference_moves >> result/heuristic_score_normal_difference_moves.txt
+echo "Running heuristic: heuristic_score_normal_difference_moves"
+touch result/heuristic_score_normal_difference_moves.txt
+python tournament.py heuristic_score_normal_difference_moves >> result/heuristic_score_normal_difference_moves.txt
 
 
-#echo "Running heuristic: heuristic_score_quadratic_difference_moves"
-#touch result/heuristic_score_quadratic_difference_moves.txt
-#python tournament.py heuristic_score_quadratic_difference_moves >> result/heuristic_score_quadratic_difference_moves.txt
+echo "Running heuristic: heuristic_score_quadratic_difference_moves"
+touch result/heuristic_score_quadratic_difference_moves.txt
+python tournament.py heuristic_score_quadratic_difference_moves >> result/heuristic_score_quadratic_difference_moves.txt
 
-
-#echo "Running heuristic: heuristic_score_min_custom_score"
-#touch result/heuristic_score_min_custom_score.txt
-#python tournament.py heuristic_score_min_custom_score >> result/heuristic_score_min_custom_score.txt
 
 
 echo "Running heuristic: heuristic_score_final_custom_score"
